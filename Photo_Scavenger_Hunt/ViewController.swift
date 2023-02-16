cf4a0d28fe4b313fc9272d80e66978bbf7eacd57
AlexRivas676 <122951701+AlexRivas676@users.noreply.github.com>
2023-02-15 23:41:54 -0800

    Initial Commit

---
+++ Photo_Scavenger_Hunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo_Scavenger_Hunt
+//
+//  Created by Alex Rivas on 2/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
